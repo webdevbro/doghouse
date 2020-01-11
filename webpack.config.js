527543b4de366228ea9e8f27f01c1ce22dfd1bf8
Ronaldo Ortiz <ronyortizop@hotmail.com>
2020-01-11 00:14:03 -0400

    completed large hero block and intro content block

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const postCSSPlugins = [
+  require("postcss-import"),
+  require("postcss-mixins"),
   require("postcss-simple-vars"),
   require("postcss-nested"),
   require("autoprefixer")
@@ -10,8 +12,16 @@ module.exports = {
     filename: "bundled.js",
     path: path.resolve(__dirname, "app")
   },
+  devServer: {
+    before: function(app, server) {
+      server._watch("./app/**/*.html")
+    },
+    contentBase: path.join(__dirname, 'app'),
+    hot: true,
+    port: 3000,
+    host: "0.0.0.0"
+  },
   mode: "development",
-  watch: true,
   module: {
     rules: [
       {
