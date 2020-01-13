d07dc5100cdd38ed0a87f5dfb04aec99594ac8a5
Ronaldo Ortiz <ronyortizop@hotmail.com>
2020-01-13 13:01:14 -0400

    Revealing elements on scroll done

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,7 @@
 import "../styles/styles.css";
 import Person from "./modules/Person";
 import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
 
 if (module.hot) {
   module.hot.accept();
@@ -9,3 +10,6 @@ if (module.hot) {
 /* lesson example code below this line */
 
 let mobileMenu = new MobileMenu();
+// let revealOnScroll = new RevealOnScroll();
+new RevealOnScroll(document.querySelectorAll(".featureItem"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 85);
